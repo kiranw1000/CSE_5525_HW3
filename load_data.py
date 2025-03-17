a6db60d245884dfac67b203859d23006970cc228
Kiran Wijesooriya <kiranw1000@gmail.com>
2025-03-17 00:28:52 -0400

    Trying something

--- load_data.py
+++ load_data.py
@@ -71,7 +71,6 @@ def normal_collate_fn(batch):
         * initial_decoder_inputs: The very first input token to be decoder (only to be used in evaluation)
     '''
     temp = [batch[i][0]['input_ids'].T for i in range(len(batch))]
-    print(temp)
     encoder_ids = torch.squeeze(pad_sequence(temp, padding_value=PAD_IDX),2).mT
     encoder_mask = torch.squeeze(pad_sequence([batch[i][0]['attention_mask'].T for i in range(len(batch))], padding_value=PAD_IDX), 2).mT
     decoder_inputs = torch.squeeze(pad_sequence([batch[i][1]['input_ids'][:,:-1].T for i in range(len(batch))], padding_value=PAD_IDX), 2).mT
@@ -93,7 +92,6 @@ def test_collate_fn(batch):
         * initial_decoder_inputs: The very first input token to be decoder (only to be used in evaluation)
     '''
     temp = [batch[i]['input_ids'].T for i in range(len(batch))]
-    print(temp)
     encoder_ids = torch.squeeze(pad_sequence(temp, padding_value=PAD_IDX), 2).mT
     encoder_mask = torch.squeeze(pad_sequence([batch[i]['attention_mask'].T for i in range(len(batch))], padding_value=PAD_IDX), 2).mT
     initial_decoder_inputs = torch.tensor([[PAD_IDX for i in range(len(batch))]]).mT
