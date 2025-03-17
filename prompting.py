947ba97abf516fa3cfcf774c2adc15af2dc4e236
Kiran Wijesooriya <kiranw1000@gmail.com>
2025-03-16 23:30:47 -0400

    Try to make it work

--- prompting.py
+++ prompting.py
@@ -79,10 +79,8 @@ def exp_kshot(tokenizer, model: GemmaForCausalLM, inputs, k, schema_path, sample
 
         input_ids = tokenizer(prompt, return_tensors="pt").to(DEVICE)
         outputs = model.generate(**input_ids, max_new_tokens=MAX_NEW_TOKENS) # You should set MAX_NEW_TOKENS
-        response = tokenizer.decode(outputs[0]) # How does the response look like? You may need to parse it
+        response = tokenizer.decode(outputs[0])[len(prompt):] # How does the response look like? You may need to parse it
         raw_outputs.append(response)
-        print(response[len(prompt):]) # You may want to print the response for each input
-        break
 
         # Extract the SQL query
         extracted_query = extract_sql_query(response)
