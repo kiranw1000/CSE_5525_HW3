2b9ea2882dc2e79be1678f06195ddde29087ca7a
Kiran Wijesooriya <kiranw1000@gmail.com>
2025-03-16 23:49:17 -0400

    Change it back

--- train_t5.py
+++ train_t5.py
@@ -227,10 +227,6 @@ def main():
     model_type = 'ft' if args.finetune else 'scr'
     checkpoint_dir = os.path.join('checkpoints', f'{model_type}_experiments', args.experiment_name)
     experiment_name = args.experiment_name
-    gt_sql_path = os.path.join(f'data/dev.sql')
-    gt_record_path = os.path.join(f'records/dev_gt_records.pkl')
-    model_sql_path = os.path.join(f'results/t5_{model_type}_{experiment_name}_dev.sql')
-    model_record_path = os.path.join(f'records/t5_{model_type}_{experiment_name}_dev.pkl')
 
     # Load the data and the model
     train_loader, dev_loader, test_loader = load_t5_data(args.batch_size, args.test_batch_size, mini=args.mini)
@@ -246,6 +242,10 @@ def main():
         model.eval()
         
         # Dev set
+        gt_sql_path = os.path.join(f'data/dev.sql')
+        gt_record_path = os.path.join(f'records/dev_gt_records.pkl')
+        model_sql_path = os.path.join(f'results/t5_{model_type}_{experiment_name}_dev.sql')
+        model_record_path = os.path.join(f'records/t5_{model_type}_{experiment_name}_dev.pkl')
         dev_loss, dev_record_em, dev_record_f1, dev_sql_em, dev_error_rate = eval_epoch(args, model, dev_loader,
                                                                                         gt_sql_path, model_sql_path,
                                                                                         gt_record_path, model_record_path)
