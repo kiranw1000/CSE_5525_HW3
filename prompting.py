399f1fae4022935cf364c5d8ff46766fb20543f1
Kiran Wijesooriya <kiranw1000@gmail.com>
2025-03-16 23:15:13 -0400

    Remove excess printing

--- prompting.py
+++ prompting.py
@@ -75,19 +75,16 @@ def exp_kshot(tokenizer, model, inputs, k, schema_path, sample_sentences, sample
     extracted_queries = []
 
     for i, sentence in tqdm(enumerate(inputs)):
-        print(sentence)
         prompt = create_prompt(sentence, k, schema_path, sample_sentences, sample_queries) # Looking at the prompt may also help
-        print(prompt)
 
         input_ids = tokenizer(prompt, return_tensors="pt").to(DEVICE)
         outputs = model.generate(**input_ids, max_new_tokens=MAX_NEW_TOKENS) # You should set MAX_NEW_TOKENS
         response = tokenizer.decode(outputs[0]) # How does the response look like? You may need to parse it
         raw_outputs.append(response)
-        print("Response: ")
-        print(response) # You may want to print the response to see if it is correct
 
         # Extract the SQL query
         extracted_query = extract_sql_query(response)
+        print(f"Extracted query: {extracted_query}")
         extracted_queries.append(extracted_query)
     return raw_outputs, extracted_queries
 
